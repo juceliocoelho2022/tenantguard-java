8ffae026f9ffe1ed18623bb7638dd298251c8056
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-09-05 18:23:20 -0300

    style: format Terraform delivery resources

--- infra/terraform/delivery.tf
+++ infra/terraform/delivery.tf
@@ -39,7 +39,7 @@ resource "aws_ecr_lifecycle_policy" "tenantguard" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url = "https://token.actions.githubusercontent.com"
 
-  client_id_list = ["sts.amazonaws.com"]
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
 }
 
